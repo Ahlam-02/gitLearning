43e15eb66b8606f956ac4aa7e36508dfaa08c5b6
Ahlam-02 <ahlaamomar1982@gmail.com>
2020-05-05 18:58:26 +0200

    Login.java

---
+++ src/tests/Login.java
@@ -0,0 +1,9 @@
+package tests;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Login {
+    public static void main(String[] args) {
+        System.out.println("should login evry time with valid credantial");
+    }
+}
