cc6900292df6671cb7630de6551b6cbf21e1a6b7
Ahlam-02 <ahlaamomar1982@gmail.com>
2020-05-06 18:30:45 +0200

    add PushGitHup.java

---
+++ src/tests/PushGitHup.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class PushGitHup {
+    public static void main(String[] args) {
+        //Task#2
+        System.out.println("Pushing code to Git Hup");
+
+        // i want to see yhis line in gitHup
+    }
+}
