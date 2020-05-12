9aa95912c1e1f91800a955d22a409f5d17ca8fec
Ahlam-02 <64778727+Ahlam-02@users.noreply.github.com>
2020-05-12 17:05:21 +0200

    Update Practice.java

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -4,6 +4,11 @@ package tests;
 
 
 public class Practice {
+    //Task 2 
+    //open github
+    //open Practice class
+    //edit in github
+    //i want to see changes in inteleji
 
     int k=1;
     String str ="i love java";
